6c0baa062cde60d6179282378f95305097fdaf4a
jpromanonet <programming@jpromano.net>
2020-05-02 01:54:23 -0300

    Rotation added

--- tetrisCPP.cpp
+++ tetrisCPP.cpp
@@ -25,6 +25,33 @@ int Rotate(int px, int py, int r) {
 	switch (r % 4) {
 		// 0 degrees
 		case 0:
+			pi = py * 4 + px;
+			break;
+		case 1:
+			pi = 12 + py - (px * 4);
+			break;
+		case 2:
+			pi = 15 - (py * 4);
+			break;
+		case 3:
+			pi = 3 - py + (px * 4);
+			break;
+	}
 
+	return pi;
+}
+
+bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY) {
+	// Let's check if every cell is full
+	for (int py = 0; py < 4; py++) {
+		for (int py = 0; py < 4; py++) {
+			// Input index into de piece
+			int pi = Rotate(px, py, nRotation);
+
+			if (nPosX + px >= 0 && nPosX + px < nFieldHeight) {
+				if (tetromino[nTetromino][pi] != L'.' && pField[fi] != 0)
+					return false;
+			}
+		}
 	}
 }
